e55274e05bcf7f669e6c0b33080c9316733ad447
Richard Silence <dune44@hotmail.com>
2020-05-25 10:48:04 -0600

    updated error schema to initial desired model.

--- schema/errors.schema.js
+++ schema/errors.schema.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/**
+ * file: Name of file where error occurred. 
+ * message: Error message. 
+ * method: The name of the function called at time of error.  
+ * processId: If available the Process Id of the service running.  
+ * variables: String form of any object of variables used.  
+ */
+
 const errorSchema = new Schema({
-    location: String,
-    error: String
+  file: String,
+  message: String,
+  method: String,
+  processId: String,
+  serverName: String,
+  stackTrace: String,
+  timestamp: String,
+  variables: String
  });
 
 module.exports = mongoose.model( 'error', errorSchema );
