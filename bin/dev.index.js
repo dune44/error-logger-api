6aa3f0973c97eac270f2d621993bc7942e258d21
Richard Silence <dune44@hotmail.com>
2020-05-29 09:19:19 -0600

    remove node_env from dev file.

--- bin/dev.index.js
+++ bin/dev.index.js
@@ -4,6 +4,6 @@ const config = require( './../etc/dev.config' );
 app.set('port', config.PORT || 1337);
 app.set('address', config.ADDRESS || 'localhost');
 const server = app.listen(app.get('port'), app.get('address'), function () {
-  if ( NODE_ENV ) console.log( 'starting as dev');
+  console.log( 'starting as dev');
   console.log('Express server listening at http://%s:%s', server.address().address, server.address().port);
 });
